424ad919d4a597c5460bfd5a46b009b69cf8e925
Itai Gendler <itai@codefresh.io>
2015-03-29 14:33:21 +0300

    fix

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,25 @@ var pg = require('pg');
 var router = express.Router();
 
 
-var conString = "postgres://username:password@localhost/database";
-var createTable = 'CREATE TABLE Persons(PersonID int,LastName varchar(255),FirstName varchar(255));';
+var conString = "postgres://itai:123456@192.168.59.103/itai";
+var createTable = 'CREATE TABLE Users(PersonID int,LastName varchar(255),FirstName varchar(255));';
+var addNewUser = "INSERT INTO users VALUES (10,'itai','gendler');";
+
+//Create the users table on start
+pg.connect(conString, function(err, client, done) {
+	if(err) {
+		return console.error('error fetching client from pool', err);
+	}
+	client.query(createTable, function(err) {
+		done();
+		if(err && (err.toString()).indexOf("already exists") > -1) {
+			console.log("Users table already exists");
+		}
+		else
+			console.log("Created Users table");
+		client.end();
+	});
+});
 
 
 /* GET users listing. */
@@ -19,11 +36,12 @@ router.get('/register', function(req, res){
 		if(err) {
 			return console.error('error fetching client from pool', err);
 		}
-		client.query(createTable, function(err, result) {
+		client.query(addNewUser, function(err, result) {
 			//call `done()` to release the client back to the pool
 			done();
 
 			if(err) {
+				console.log(err);
 				return res.send('error', err);
 			}
 			//console.log(result.rows[0].number);
